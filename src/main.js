a747e71d76adbfb4348c11672c8273b7f12340d0
metappi <paperdju2@gmail.com>
2026-07-21 23:55:38 +0900

    doc:CSS調整

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
-// src/main.js（起動係。この3行が「App.vueをid=appに差し込め」の意味）
+// Vue.js本体からcreateApp()関数をインポート 
 import { createApp } from 'vue'
+// これまでコーディングをしてきたApp.vueファイルをAppとしてインポート
 import App from './App.vue'
+// createApp()はVueアプリケーションを作成する処理でここには
+// 起点となる単一コンポーネントファイル（src）で渡す
+// mount()はcreateApp()で作ったVueアプリケーションを表示するメソッド
+// index.htmlのどのタグに表示するのかを引数で指定
+// この場合は#appとあり、これがまさに先ほどのidがappのdivタグ内に
+// レンダリングされて表示されるという仕組み
 createApp(App).mount('#app')
